1d3e80b63e39888029558109339a029e3a341c86
AfranurM <afranurmenemenli1103@gmail.com>
2022-09-06 14:30:52 -0400

    Day2

--- src/test/java/StepDefinition/Day2_WebAppSecurityFunctionalitiesTest.java
+++ src/test/java/StepDefinition/Day2_WebAppSecurityFunctionalitiesTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
@@ -98,5 +99,64 @@ public class Day2_WebAppSecurityFunctionalitiesTest {
         }
 
         driver.quit();
+    }
+
+    @When("User navigates to Purchase Foreign Currency Section")
+    public void user_navigates_to_purchase_foreign_currency_section() {
+
+        wait = new WebDriverWait(driver, 10);
+
+        WebElement onlineBankingButton = driver.findElement(By.cssSelector("#onlineBankingMenu>div>strong"));
+        onlineBankingButton.click();
+        wait.until(ExpectedConditions.urlContains("online-banking"));
+
+        WebElement payBillsButton =driver.findElement(By.id("pay_bills_link"));
+        payBillsButton.click();
+        wait.until(ExpectedConditions.urlContains("pay-bills"));
+
+        WebElement purchaseForeignCurrency = driver.findElement(By.linkText("Purchase Foreign Currency"));
+        purchaseForeignCurrency.click();
+
+    }
+    @When("User select {string} and select {string} and select USD radio button")
+    public void user_select_and_select_and_select(String currency, String amount) {
+
+        WebElement usdButton = driver.findElement(By.id("pc_inDollars_true"));
+        usdButton.click();
+
+        WebElement currencyDropdown = driver.findElement(By.id("pc_currency"));
+        Select selectCurrency = new Select(currencyDropdown);
+
+        WebElement amountInput = driver.findElement(By.id("pc_amount"));
+
+        if ((currency.equals("China"))&&(amount.equals("500"))){
+            selectCurrency.selectByVisibleText("China (yuan)");
+            amountInput.sendKeys("500");
+        }else if((currency.equals("Norway"))&&(amount.equals("200"))){
+            selectCurrency.selectByVisibleText("Norway (krone)");
+            amountInput.sendKeys("200");
+        }else if((currency.equals("Thailand (baht)"))&&(amount.equals("300"))){
+            selectCurrency.selectByVisibleText("Thailand (baht)");
+            amountInput.sendKeys("300");
+        }
+
+        WebElement purchaseButton = driver.findElement(By.id("purchase_cash"));
+        purchaseButton.click();
+
+    }
+
+    @Then("User should successfully purchase foreign currency")
+    public void user_should_successfully_purchase_foreign_currency() {
+
+        wait = new WebDriverWait(driver, 10);
+
+        WebElement successMessage = driver.findElement(By.id("alert_content"));
+        Assert.assertTrue(successMessage.isDisplayed());
+
+        WebElement makePaymentsMessage = driver.findElement(By.className("board-header"));
+        Assert.assertTrue(makePaymentsMessage.isDisplayed());
+
+        driver.quit();
+
     }
 }
